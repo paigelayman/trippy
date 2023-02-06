c15acd004dacab1054d6a67dfb1d0470149ec33a
Paige Layman <paigelayman96@gmail.com>
2023-02-06 12:31:19 -0500

    .env set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import logo from "./logo.svg"
-import "./App.css"
+import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <h1>TRIPPYYY</h1>
+    </div>
+  )
 }
 
 export default App
