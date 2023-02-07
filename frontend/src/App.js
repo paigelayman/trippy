80eec3139eb4485646356826d602f89e7014bee8
David Caldwell <doubledc92@gmail.com>
2023-02-07 13:35:19 -0500

    adding pages, components, and images files to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
-import "./App.css"
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Nav from './components/Nav'
+import Home from './pages/Home'
+import About from './pages/About'
+import UserDetails from './pages/UserDetails'
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <header className="navBar">
+        <Nav />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users/:id" element={<UserDetails />} />
+        </Routes>
+      </main>
     </div>
   )
 }
